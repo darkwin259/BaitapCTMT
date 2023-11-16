3488fd36ab3bb63454e576bb4fc9c8065cf84a7c
HoangAnhTuan <89626694+darkwin259@users.noreply.github.com>
2023-11-16 15:14:44 +0700

    Add files via upload

---
+++ baitap1.asm
@@ -0,0 +1,37 @@
+org 100h
+mov al,03h
+mov ah,0
+int 10h
+mov dl,10
+mov dh,1
+mov ah,2
+int 10h
+mov al,1
+mov bh,0
+mov bl,01001111b 
+mov cx,1    
+lea bp,str
+mov ah,13  
+mov di,0
+lea bp,str+di
+mov ah,13h
+int 10h
+lap:
+inc di 
+inc dh 
+mov ah,2
+int 10h
+mov dl, 0Ah 
+int 21h  
+mov al,1
+mov bh,0
+mov bl,01001111b 
+mov cx,1 
+lea bp,str+di
+mov ah,13h
+int 10h
+cmp dh,15
+jnz lap  
+je endss
+str db 'Lop ten lua k57'
+endss:
